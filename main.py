4a25b52424c88d925c24f2e489d35dc42a3421c4
Ibrahim.Chaibedraa <chaibedraa.work@gmail.com>
2021-01-10 13:23:28 +0100

    Extracting edges

--- main.py
+++ main.py
@@ -37,33 +37,40 @@ savel.pack(side=TOP, pady=50)
 
 # Storing the Image
 def cartoonify(image_path):
-    # Read image
-    # Imread is a method in cv2 which is used to store images in the form of numbers
-    original_image = cv2.imread(image_path)
-    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    print(original_image) # this will be stored in form of number
+	# Read image
+	# Imread is a method in cv2 which is used to store images in the form of numbers
+	original_image = cv2.imread(image_path)
+	original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+	print(original_image) # this will be stored in form of number
 
 	# To confirm if it is an image that was chosing
-    if original_image is None:
-    	print("Can’t find any image! Choose appropriate file. ")
-    	sys.exit()
-    # Resize the image after each transformation
-    # We resize the image after each transformation to display all the images on a similar scale at last
-    resize_image1 = cv2.resize(original_image, (960, 540))
-    plt.imshow(resize_image1, cmap='gray')
+	if original_image is None:
+		print("Can’t find any image! Choose appropriate file. ")
+		sys.exit()
+	# Resize the image after each transformation
+	# We resize the image after each transformation to display all the images on a similar scale at last
+	resize_image1 = cv2.resize(original_image, (960, 540))
+	plt.imshow(resize_image1, cmap='gray')
 
 	# Transform Image to grayscale
 	# Our first step is to convert the image into grayscale
-    grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+	grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 	# cvtColor(image, flag) is a method in cv2 which is used to transform an image into the colour-space mentioned as ‘flag’
 	# we use the BGR2GRAY flag. This returns the image in grayscale
 
 	# After each transformation, we resize the resultant image using the resize() method in cv2
 	# This is done to get more clear insights into every single transformation step
-    resize_image2 = cv2.resize(grayscale_image, (960, 540))
+	resize_image2 = cv2.resize(grayscale_image, (960, 540))
 	# And display it using imshow() method
-    plt.imshow(resize_image2, cmap="gray")
-	
+	plt.imshow(resize_image2, cmap="gray")
+
+	# Smoothening a grayscale image
+	smooth_grayscale_image = cv2.medianBlur(grayscale_image, 5)
+	# The medianBlur() function from cv2 is used in smoothening the grayscale image
+	resize_image3 = cv2.resize(smooth_grayscale_image, (960, 540))
+	plt.imshow(resize_image3, cmap="gray")
+
+
 
 
 
